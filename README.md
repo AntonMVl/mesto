fcf34875eebfcbd2f94a7568a70faf61c566d63e
Anton Morohin <AMorohin@yandex.ru>
2023-03-20 22:27:20 +0300

    first rev

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 ### Обзор
 
+* HTML
+* CSS
+* JavaScript
+* BEM
 * Figma
 * Картинки
 
-**Figma**
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+**gh-pages**
 
-**Картинки**
+* [Ссылка на проект в gh-pages](https://antonmvl.github.io/mesto/)
 
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
 
-Удачи!
--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
-const popupOpenButton = document.querySelector('.profile__editing-button-link');
+const popupOpenButton = document.querySelector('.profile__editing-button');
 const userInfoPopup = document.querySelector('.popup_type_user-info');
 const userInfoPopupClose = userInfoPopup.querySelector('.popup__close');
 const userInfoPopupForm = userInfoPopup.querySelector('.popup__form');
-const userInfoPopupInputName = userInfoPopup.querySelector('.popup__input-name');
-const userInfoPopupInputJob = userInfoPopup.querySelector('.popup__input-job');
-const userInfoPopupSubmit = userInfoPopup.querySelector('.popup__input-button');
+const userInfoPopupInputName = userInfoPopup.querySelector('.popup__form-input_add_name');
+const userInfoPopupInputJob = userInfoPopup.querySelector('.popup__form-input_add_job');
 const userNameValue = document.querySelector('.profile__name');
 const userJobValue = document.querySelector('.profile__job-title');
 
 
 
 function pressClick () {
-    userInfoPopup.classList.add('popup_open');
+    userInfoPopup.classList.add('popup_opened');
     userInfoPopupInputName.value = userNameValue.textContent;
     userInfoPopupInputJob.value = userJobValue.textContent;
 }
@@ -19,18 +18,16 @@ function pressClick () {
 popupOpenButton.addEventListener('click', pressClick);
 
 function pressCloseButton () {
-    userInfoPopup.classList.remove('popup_open');
+    userInfoPopup.classList.remove('popup_opened');
 };
 
 userInfoPopupClose.addEventListener('click', pressCloseButton);
 
 function submitUserInfo (event) {
     event.preventDefault();
-    const inputName = userInfoPopupInputName.value;
-    userNameValue.textContent = inputName;
-    const inputJob = userInfoPopupInputJob.value;
-    userJobValue.textContent = inputJob;
-    userInfoPopup.classList.remove('popup_open');
+    userNameValue.textContent = userInfoPopupInputName.value;
+    userJobValue.textContent = userInfoPopupInputJob.value;
+    pressCloseButton();
 };
 
 userInfoPopupForm.addEventListener('submit', submitUserInfo);
